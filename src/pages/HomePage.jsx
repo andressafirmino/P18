4b819abdcd2629f7bfd29d75fa99e2954cb670a8
andressafirmino <andressafirmino78@gmail.com>
2023-08-13 23:15:15 -0300

    feat: layout e requisição GET/home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,7 +16,8 @@ export default function HomePage() {
             headers: { authorization: `Bearer ${token}` }
         })
             .then((res) => {
-              console.log(res);
+              console.log(res.data.products);
+              setAll(res.data.products);
             })
             .catch((err) => {
                 console.log(err.message)
@@ -37,16 +38,16 @@ export default function HomePage() {
                 {all && (
                     <>
                         {all.map(l =>
-                            <BoxLink key={l.id}>
-                                <Link>
-                                    <p className="size" onClick={() => openLink(l.shortUrl, l.url)}>{l.url}</p>
-                                    <p onClick={() => openLink(l.shortUrl, l.url)}>{l.shortUrl}</p>
-                                    <p className="size">Quantidade de visitantes: {l.visitCount}</p>
-                                </Link>
-                                <TrashStyled>
-                                    <img src={Trash} onClick={() => deleteUrl(l.id)} />
-                                </TrashStyled>
-                            </BoxLink>
+                            <CsProduct key={l.idProduct}>
+                            <img src={l.photos[0].photo}  onClick={()=>displayProduct(l.category, l.idProduct)}/>
+                            <h2  onClick={()=>displayProduct(l.category, l.idProduct)}>{l.nameProduct}</h2>
+                            <div className="valuesProduct"  onClick={()=>displayProduct(l.category, l.idProduct)}> {l.category}</div>
+                            
+                            
+                                <button className="addCard">Fale com o vendedor</button>
+                            
+                            
+                        </CsProduct>
                         )}
                     </>
                 )}
@@ -60,13 +61,59 @@ const HomeContainer = styled.div`
   height: auto;
   width: 100hv;
   display: flex;
-  flex-direction: column;
-
+  justify-content: center;
+  flex-wrap: wrap;
   main{
     width: 100hv;
     height: auto;
     display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
+    
   }
+`;
+const CsProduct = styled.div`
+    width: 200px;
+    height: 300px;
+    margin: 20px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    border: 3px solid #73384E;
+    border-radius: 5px;
+
+    img{
+        width: 100%;
+        height: 85%;
+        cursor: pointer;
+        border-radius: 12px;
+        transition: transform 0.3s;
+        &:hover {
+            transform: scale(1.05);
+        }
+    }
+    h2{
+        overflow-y: hidden;
+        max-height: 100px;
+        cursor: pointer;
+    }
+    .valuesProduct{
+        margin-top: 3px; 
+        cursor: pointer;
+
+    }
+    .addCard{
+        width: 100%;
+        height: 35px;
+        margin-top: 3px;
+    }
+    button{
+        cursor: pointer;
+        background-color: #FF3EB5;
+        color: white;
+        border-radius: 5px;
+        border: none;
+        transition: transform 0.3s;
+        &:hover {
+            transform: scale(1.1);
+        }
+    }
 `;
